c34903499b0d65ff05d842de915199b4d3280c24
hyun6ik <fkanwm@naver.com>
2021-01-07 00:55:10 +0900

    조건식 : case, coalesce, nullif

--- src/main/java/hellojpql/JpaMain.java
+++ src/main/java/hellojpql/JpaMain.java
@@ -32,15 +32,42 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select m.name, 'HELLO', TRUE From Member m where m.type = :userType";
-            List<Object[]> resultList = em.createQuery(query)
-                    .setParameter("userType", MemberType.ADMIN)
+            String query = "select nullif(m.name, '관리자') as name from Member m";
+            List<String> result = em.createQuery(query, String.class)
                     .getResultList();
-            for (Object[] objects : resultList) {
-                System.out.println("objects = " + objects[0]);
-                System.out.println("objects = " + objects[1]);
-                System.out.println("objects = " + objects[2]);
+            for (String s : result) {
+                System.out.println("s = " + s);
             }
+
+//            String query = "select coalesce(m.name, '이름 없는 회원') as name from Member m";
+//            List<String> result = em.createQuery(query, String.class)
+//                    .getResultList();
+//            for (String s : result) {
+//                System.out.println("s = " + s);
+//            }
+
+//            String query =
+//                    "select " +
+//                            "case when m.age <=10 then '학생요금'" +
+//                            "     when m.age >=60 then '경로요금'" +
+//                            "     else '일반요금'" +
+//                    "end from Member m";
+//            List<String> resultList = em.createQuery(query, String.class)
+//                    .getResultList();
+//            for (String s : resultList) {
+//                System.out.println("s = " + s);
+//            }
+
+
+//            String query = "select m.name, 'HELLO', TRUE From Member m where m.type = :userType";
+//            List<Object[]> resultList = em.createQuery(query)
+//                    .setParameter("userType", MemberType.ADMIN)
+//                    .getResultList();
+//            for (Object[] objects : resultList) {
+//                System.out.println("objects = " + objects[0]);
+//                System.out.println("objects = " + objects[1]);
+//                System.out.println("objects = " + objects[2]);
+//            }
 
 
 //            String query = "select m from Member m left join m.team t on t.name = 'teamA'";
